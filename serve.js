a526ad261b0a127f099daee6946fa0d729eefd76
okuoku <mjt@cltn.org>
2018-12-10 05:30:19 +0900

    Init

---
+++ serve.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const builder = require("./node_modules/@yuniscm/yuniappjs-pkg/scripts/parcel-run.js");
+const path = require("path");
+
+const cwd = process.cwd();
+
+function subpath(name){
+    return path.join(cwd, name);
+}
+
+var config = {
+    buildtype: "debug",
+    yuniroot: subpath("/yuni"),
+    libpath: [subpath("/yunilib"),subpath("/rs-component-proto/yunilib")],
+    progs: [subpath("/app.sps")]
+};
+
+builder.run(config);
